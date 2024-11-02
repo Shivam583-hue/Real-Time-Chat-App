131e9d419f2c42fdf18e99c40a25d260e658c3e9
Shivam915201 <shivamkumarsinghdragon1@gmail.com>
2024-11-03 02:47:19 +0530

    last commit?

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route"
@@ -11,11 +13,18 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
 
 // app.get('/',(req,res)=> {
 //     res.send("Server is running")
