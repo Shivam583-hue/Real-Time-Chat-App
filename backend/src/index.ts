b58952df0f1736a8e903439d542401edd382c0a8
Shivam915201 <shivamkumarsinghdragon1@gmail.com>
2024-11-03 04:49:38 +0530

    final tweak

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use("/api/users",userRoutes)
 app.use(express.static(path.join(__dirname,"frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))x
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
 })
 
 // app.get('/',(req,res)=> {
